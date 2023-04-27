790963c2f412ae93dc0b0d8c38beb32f44bc7cd5
meru_zh <meruertzhunusova1711@gmail.com>
2023-04-28 00:57:52 +0600

    Implement FollowerCell

---
+++ Github followers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,32 @@
+//
+//  GFAvatarImageView.swift
+//  Github followers
+//
+//  Created by Meruert on 28.04.2023.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+        
+    }
+    
+
+}
