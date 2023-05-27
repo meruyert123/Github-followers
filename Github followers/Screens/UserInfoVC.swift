d369b4a860c20e782d08b5a28a95309ce07824a2
meruyert123 <meruertzhunusova1711@gmail.com>
2023-05-27 21:51:04 +0600

    Add date label in User Info view controller

--- Github followers/Screens/UserInfoVC.swift
+++ Github followers/Screens/UserInfoVC.swift
@@ -32,6 +32,7 @@ class UserInfoVC: UIViewController {
                     self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
                     self.add(childVC: GFRepoItemVC(user: user), to: self.itemViewOne)
                     self.add(childVC: GFFollowerItemVC(user: user), to: self.itemViewTwo)
+                    self.dateLabel.text = "Github since \(user.createdAt.convertToDisplayFormat())"
                 }
             case .failure(let error) :
                 self.presentGFAlertOnMainThread(title: "Something get wrong", message: error.rawValue, buttonTitle: "Ok")
