adaad75060880d1f799e9b789ec05a04bed35ca3
meruyert123 <meruertzhunusova1711@gmail.com>
2023-05-27 21:07:10 +0600

    Initialize ItemInfo view controller with User

--- Github followers/Custom Views/ViewControllers/IItemInfoVC/GFItemInfoVC.swift
+++ Github followers/Custom Views/ViewControllers/IItemInfoVC/GFItemInfoVC.swift
@@ -1,12 +1,23 @@
 import UIKit
 
-class GFItemInfoViewController: UIViewController {
+class GFItemInfoVC: UIViewController {
     
     let stackView = UIStackView()
     let itemInfoViewOne = GFItemInfoView()
     let itemInfoViewTwo = GFItemInfoView()
     let actionButton = GFButton()
 
+    var user: User!
+    
+    init(user: User) {
+        super.init(nibName: nil, bundle: nil)
+        self.user = user
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureBackgroundView()
@@ -31,6 +42,8 @@ class GFItemInfoViewController: UIViewController {
         view.addSubview(stackView)
         view.addSubview(actionButton)
         
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
         let padding: CGFloat = 20
         
         NSLayoutConstraint.activate([
@@ -39,10 +52,10 @@ class GFItemInfoViewController: UIViewController {
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
             stackView.heightAnchor.constraint(equalToConstant: 50),
             
-            actionButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: -padding),
+            actionButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -padding),
             actionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
             actionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
-            actionButton.heightAnchor.constraint(equalToConstant: 44),
+            actionButton.heightAnchor.constraint(equalToConstant: 44)
         ])
     }
 }
