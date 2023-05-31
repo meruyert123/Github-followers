67be745780e45119d6c18d92e26a36739a4ac30a
meruyert123 <meruertzhunusova1711@gmail.com>
2023-05-31 12:19:36 +0600

    Implement add button in FollowerList view controller

--- Github followers/Screens/FollowerListVC.swift
+++ Github followers/Screens/FollowerListVC.swift
@@ -112,7 +112,31 @@ class FollowerListVC: UIViewController {
     }
     
     @objc func addButtonTapped() {
+        showLoadingView()
         
+        NetworkManager.shared.getUserInfo(for: username) { [weak self] result in
+            guard let self = self else { return }
+            self.dismissLoading()
+            
+            switch result {
+            case .success(let user):
+                let favorite = Follower(login: user.login, avatarUrl: user.avatarUrl)
+                
+                PersistenceManager.updateWith(favorite: favorite, actionType: .add) { [weak self] error in
+                    guard let self = self else { return }
+                    
+                    guard let error = error else {
+                        self.presentGFAlertOnMainThread(title: "Success!", message: "You have successfully favorite this user!", buttonTitle: "Ok")
+                        return
+                    }
+                    
+                    self.presentGFAlertOnMainThread(title: "Something get wrong.", message: error.rawValue, buttonTitle: "Ok")
+                }
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(title: "Something get wrong.", message: error.rawValue, buttonTitle: "Ok")
+            }
+        }
     }
 }
 
